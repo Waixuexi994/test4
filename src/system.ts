8f9b3cdb44b6be25b4520831a8c501a6ae7acab1
waixuexi <59311567+Waixuexi994@users.noreply.github.com>
2023-04-16 23:32:09 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,8 +10,9 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `书虫
-- 网站以进行升级，新的网站添加VX：SCFuYe003，朋友圈有免费的可以使用
+export const defaultMessage = `微信公众号：书虫创客
+- 网站以进行更改，现在网站不需要填写key即可使用，网站余额没了或者有其他问题添加书虫微信：SCFuYe003
+- 关注微信公众号：书虫创客，可以了解ChatGPT以及AI最新咨询
 - [[Shift]] + [[Enter]] 换行。开头输入 [[空格]] 跳出更多 预设。[[↑]] 可编辑最近一次提问。`
 
 export type Setting = typeof defaultSetting
